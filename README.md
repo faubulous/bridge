1f0ca4b23371d2f79cd7718656e7e9fbbcb9404f
Sebastian Faubel <sebastian@semiodesk.com>
2023-02-12 14:39:34 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,77 @@
+# Bridge of Enlightenment
+
+The bridge of enlightenment is an art project initated by [Gregor Faubel](https://www.studio-faubel.de) 
+in cooperation with Julius Faubel. It is a bridge with many arcs which are illuminated when a metal ball
+is passing them. The bridge infinitely rolls the ball from one side to the other. The main idea behind this project is:
+
+<blockquote>
+On the road to enlightenment one must constantly take different points of view in order to see the bigger picture.
+</blockquote>
+
+## Getting Started
+
+This repository contains the source code of the Arduino controller. It can be compiled with 
+[Arduino Studio 2.0](https://docs.arduino.cc/software/ide-v2/tutorials/getting-started/ide-v2-downloading-and-installing) 
+and does not require any external libraries.
+
+1. Checkout or download this repository into a folder named `bridge`
+1. Open the `bridge.ino` file in Arduino Studio
+1. Hit `CTRL + R` to compile the source code
+
+To upload the code you must do the following:
+
+1. Attach the Arduino board via USB
+1. Select the `Arduino Uno` board in the [Arduino Studio Board Selector](https://support.arduino.cc/hc/en-us/articles/4406856349970-Select-board-and-port-in-Arduino-IDE).
+1. Hit `CTRL + U`to compile and upload the code
+
+Note: On Linux you must ensure that your user has write privileges to the USB device `/dev/usb/tty0`.
+
+## Making Changes
+The timing of the bridge movement is delicate but essential for the ball not to become to fast or to get stuck along the way. In order to make changes to the timing you can change the following lines of code:
+
+```
+// Defines potentiometer values that should be moved to in the given sequence.
+// When the last value was reached the list will skip the first value and repeat from the second.
+int targetList[5] = { 0, 160, 50, -160, -50 };
+
+// Defines delays in ms that should be waited after a target was reached. The
+// sequence corresponds to the values in the above target list.
+int targetDelay[5] = { 5000, 0, 7200, 0, 7200 };
+```
+
+### Changing timing values
+The `targetList` variable contains the potentiometer values which represent the relative offset in *mm* from the levelled or zero position if the brige in each step.
+
+The bridge moves to the first step and once the programmed value was reached will wait for the time in *ms* specified by the corresponding `targetDelay` variable.
+
+Once this is done, we move to the next step until we reach the last target. After reaching the last target the controller returns to the second value in the list, skipping the inital zero target.
+
+### Adding or removing value
+When altering the list one must ensure that the array size of both, the `targetList[5]` and `targetDelay[5]` match the number of values and are equal.
+
+## Notes
+### Stepper Initalization Pin Order
+The wiring of the used `28BYJ 48` stepper motor does not match the standard (BLUE, PINK, RED, ORANGE, YELLOW).
+
+<img src="https://www.electrokit.com/uploads/productimage/41014/41014801.png" width="400">
+
+Instead it is wired like this:
+
+<img src="https://www.makerfabs.com/image/cache/makerfabs/28BYJ-48%20Stepper%20motor-5V/28BYJ-48%20Stepper%20motor-5V_3-1000x750.JPG" width="400">
+
+This is why the Stepper motor is initialized in the following sequence:
+
+```
+Stepper stepper(STEPS_PER_REVOLUTION, 9, 11, 8, 10);
+```
+
+As opposed to the sequence mentioned in most articles:
+
+```
+Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
+```
+
+## Links
+
+- https://community.element14.com/members-area/personalblogs/b/blog/posts/arduino-controlling-stepper-motor-28byj-48-with-accelstepper-library
+- https://lastminuteengineers.com/28byj48-stepper-motor-arduino-tutorial/
--- bridge.ino
+++ bridge.ino
@@ -1,26 +1,42 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 
 const int LED_PIN = 13;
 
-// Taken from here: https://arduinogetstarted.com/tutorials/arduino-controls-28byj-48-stepper-motor-using-uln2003-driver
-
-// Number taken from here: https://www.makerguides.com/28byj-48-stepper-motor-arduino-tutorial/
+// See: https://arduinogetstarted.com/tutorials/arduino-controls-28byj-48-stepper-motor-using-uln2003-driver
+// See: https://www.makerguides.com/28byj-48-stepper-motor-arduino-tutorial/
 const int STEPS_PER_REVOLUTION = 2048;
 
 // Note: The order of pins is adapted to the *actual* wiring on the stepper
 // motor and differs from the order describe in the previous articles.
 Stepper stepper(STEPS_PER_REVOLUTION, 9, 11, 8, 10);
 
+// Potentiometer value when the bridge is levelled.
+int center = 268;
+
+// Defines potentiometer values that should be moved to in the given sequence.
+// When the last value was reached the list will skip the first value and repeat from the second.
+int targetList[5] = { 0, 160, 50, -160, -50 };
+
+// Defines delays in ms that should be waited after a target was reached. The
+// sequence corresponds to the values in the above target list.
+int targetDelay[5] = { 5000, 0, 7200, 0, 7200 };
+
+// Index from 0 to len(targetList) - 1 of the currently selected target.
+int targetIndex = 0;
+
+// Current value of the potentiometer mapped to mm above 0.
 int position = 0;
 
-int target = 268;
+// Current distance from the current target in mm. 
+int distance = 0;
 
-int threshold = 5;
+// Tolerance in mm when we consider a target to be reached.
+int threshold = 3;
 
-void setup(){
+void setup()
+{
   // Set the built-in LED pin as output.
   pinMode(LED_PIN, OUTPUT);
 
@@ -35,48 +51,75 @@ void setup(){
   Serial.println("Initialized.");
 }
 
-void loop() {
-  bool levelled = updatePosition();
-
-  if(!levelled) {
-    digitalWrite(LED_PIN, LOW);
-  } else {
-    digitalWrite(LED_PIN, HIGH);
-
-    Serial.println("LEVELLED");
-  }
-
+void loop()
+{
+  updatePosition();
+  updateDistance();
   step();
 }
 
-bool updatePosition() {
+void updatePosition()
+{
   // Map the 8 Bit analog value to the 6cm scale of the potentiometer.
   position = map(analogRead(A0), 0, 1024, 0, 600);
 
   Serial.print("position: ");
-  Serial.println(position);
+  Serial.print(position);
 
-  int center = 300;
+  bool levelled = (center - threshold) < position && position < (center + threshold);
 
-  return (center - threshold) < position && position < (center + threshold);
+  if(!levelled)
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
 }
 
-void step() {
-  int distance = target - position;
+void updateDistance()
+{
+  int t = center + targetList[targetIndex];
+  int d = t - position;
 
-  Serial.print("step: ");
-  Serial.println(distance);
+  Serial.print(" distance: ");
+  Serial.println(d);
 
-  if(abs(distance) >= threshold)
+  distance = d;
+}
+
+void step()
+{
+  if(abs(distance) > threshold)
   {
+    // Get the required direction towards the target; -1 is CCW and 1 is CW.
     int d = distance > 0 ? -1 : 1;
     int s = d * min(100, abs(distance));
 
     // This is the speed I found to be most reliable. With higher
-    // speeds the motor has hick-ups when running counter clock wise.
+    // speeds the motor has hickups when running counter clock wise (CCW).
     stepper.setSpeed(11);
     stepper.step(s);
 
     position += s;
+  }
+  else
+  {
+    // Wait the defined number of ms for the current target..
+    int ms = targetDelay[targetIndex];
+
+    delay(ms);
+
+    // C++ is ugly and we need to calculate the size of the target list.
+    int targetCount = sizeof(targetList) / sizeof(targetList[0]);
+
+    targetIndex += 1;
+
+    // If we reached the last target then restart and skip the intial center.
+    if(targetIndex > (targetCount - 1))
+    {
+      targetIndex = 1;
+    }
   }
 }
